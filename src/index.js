4e3850697bb1ea472d3a3985e08f4ec062167d8d
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-06-03 00:38:57 +0100

    pokemon completed!

--- src/index.js
+++ src/index.js
@@ -10,36 +10,38 @@ const cardsUl = document.querySelector(".cards");
 // going through the data of pokemon
 for (let i = 0; i < data.length; i++) {
   const pokemon = data[i];
-
-  // create a li with the pokemnon name
   const cardLi = document.createElement("li");
-  // li.textContent = pokemon.name;
   cardLi.classList.add("card");
   cardsUl.appendChild(cardLi);
+
   // create h2 with class card-title
   const h2 = document.createElement("h2");
   h2.textContent = pokemon.name;
+  h2.classList.add("card--title");
   cardLi.appendChild(h2);
+
   //  Create img element with class name card--img
   const img = document.createElement("img");
   img.src = pokemon.sprites.front_default;
   img.classList.add("card--img");
   cardLi.appendChild(img);
+
   // create UL with card--text
   const cardTextUl = document.createElement("ul");
-  cardTextUl.classList.add("card-text");
+  cardTextUl.classList.add("card--text");
   cardLi.appendChild(cardTextUl);
 
-  // select cardTxtUl
- 
-  let HP
+  // select cardTextUl
+
   const stats = pokemon.stats;
-  
-  for (let j = 0 ; j < stats.length; j++) {
-    // cardTextUl.appendChild(li);
+  console.log(stats, "stats");
+
+  for (let j = 0; j < stats.length; j++) {
     const li = document.createElement("li");
-      HP = stats[j].base_stat;
-    li.textContent = 'HP: ' + HP
-    cardLi.appendChild(li)
-    }
+    cardTextUl.appendChild(li);
+    let rating = stats[j].base_stat;
+    let name = stats[j].stat.name;
+    console.log("NAME", name);
+    li.textContent = name + ": " + rating;
+  }
 }
